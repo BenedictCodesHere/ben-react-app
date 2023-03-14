7b4cc653d417b38e0c552ed725706edb98c5f601
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-03-14 04:40:23 +0000

    updated button styling

--- src/components/ProjectNavbar.js
+++ src/components/ProjectNavbar.js
@@ -6,12 +6,12 @@ const ProjectNavbar = () => {
 
   return (
     <div className="navbar row">
-      <Link className="btn btn-block" to="/projects/french-tutoring-website" key={1} ><i className="bi bi-award"></i>  French Tutoring Website  <i className="bi bi-award"></i></Link>
-      <Link className="btn btn-block" to="/projects/code-quiz" key={2}><i class="bi bi-clipboard2-check-fill"></i>  Code Quiz  <i class="bi bi-clipboard2-x-fill"></i></Link>
-      <Link className="btn btn-block" to="/projects/weather-dashboard" key={3}><i className="bi bi-cloud-drizzle"></i>   Weather Dashboard   <i className="bi bi-cloud-drizzle"></i></Link>
-      <Link className="btn btn-block" to="/projects/daily-planner" key={4}><i className="bi bi-journal-bookmark-fill"></i>  Daily Planner  <i className="bi bi-journal-bookmark-fill"></i></Link>
-      <Link className="btn btn-block" to="/projects/password-generator" key={5}><i className="bi bi-asterisk"></i>  Password Generator  <i className="bi bi-asterisk"></i></Link>
-      <Link className="btn btn-block" to="/projects/README-generator" key={6}><i className="bi bi-card-list"></i>  README Generator  <i className="bi bi-card-list"></i></Link>
+      <Link className="btn btn-block btn-project" to="/projects/french-tutoring-website" key={1} ><i className="bi bi-award"></i>  French Tutoring Website  <i className="bi bi-award"></i></Link>
+      <Link className="btn btn-block btn-project" to="/projects/code-quiz" key={2}><i class="bi bi-clipboard2-check-fill"></i>  Code Quiz  <i class="bi bi-clipboard2-x-fill"></i></Link>
+      <Link className="btn btn-block btn-project" to="/projects/weather-dashboard" key={3}><i className="bi bi-cloud-drizzle"></i>   Weather Dashboard   <i className="bi bi-cloud-drizzle"></i></Link>
+      <Link className="btn btn-block btn-project" to="/projects/daily-planner" key={4}><i className="bi bi-journal-bookmark-fill"></i>  Daily Planner  <i className="bi bi-journal-bookmark-fill"></i></Link>
+      <Link className="btn btn-block btn-project" to="/projects/password-generator" key={5}><i className="bi bi-asterisk"></i>  Password Generator  <i className="bi bi-asterisk"></i></Link>
+      <Link className="btn btn-block btn-project" to="/projects/README-generator" key={6}><i className="bi bi-card-list"></i>  README Generator  <i className="bi bi-card-list"></i></Link>
     </div>
   );
 };
