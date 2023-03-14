b41fde9424bb640ccac62941f102b41bae2c744b
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-03-14 00:43:06 +0000

    updated CV link

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
     <div className="row">
         <div className="col-sm-8 col-md-8 col-lg-8"></div>
         <p className="text-center my-5">Email:<br></br> codingwithbenedict99@gmail.com</p>
-        <Link className="btn btn-info" to={{pathname:"https://github.com/BenedictCodesHere/"}}>CV</Link>
+        <a className="btn btn-info" href="https://github.com/BenedictCodesHere/ben-react-app/blob/main/src/assets/CV/Benedict-McElroy-CV.pdf" target="_blank">CV</a>
         <Link className="btn btn-warning" to="https://github.com/BenedictCodesHere/">Github</Link>
         <Link className="btn btn-info" to="https://www.linkedin.com/in/benedict-mcelroy-005256230">LinkedIn</Link>
     </div>
