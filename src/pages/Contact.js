e39997f64376e311051e4ece4bdfbb5dfe85f3eb
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-03-14 04:20:42 +0000

    updated style

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,10 +6,10 @@ function Contact() {
 <><div className="container">
     <div className="row">
         <div className="col-sm-8 col-md-8 col-lg-8"></div>
-        <p className="text-center email my-5">Email:<br></br> codingwithbenedict99@gmail.com</p>
-        <a className="btn btn-info" href="https://github.com/BenedictCodesHere/ben-react-app/blob/main/src/assets/CV/Benedict-McElroy-CV.pdf" target="_blank">CV</a>
-        <Link className="btn btn-warning" to="https://github.com/BenedictCodesHere/">Github</Link>
-        <Link className="btn btn-info" to="https://www.linkedin.com/in/benedict-mcelroy-005256230">LinkedIn</Link>
+        <p className="text-center email my-5"><i class="bi bi-envelope-at"></i><br></br> codingwithbenedict99@gmail.com</p>
+        <a className="btn" href="https://github.com/BenedictCodesHere/ben-react-app/blob/main/src/assets/CV/Benedict-McElroy-CV.pdf" target="_blank">CV</a>
+        <Link className="btn" to="https://github.com/BenedictCodesHere/"><i class="bi bi-github"></i></Link>
+        <Link className="btn" to="https://www.linkedin.com/in/benedict-mcelroy-005256230"><i class="bi bi-linkedin"></i></Link>
     </div>
   </div></>
     )
