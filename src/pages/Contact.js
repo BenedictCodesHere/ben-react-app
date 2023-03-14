8829a3051200c079e70ec5a72243a778a39c484d
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-03-14 14:12:17 +0000

    updated contact buttons styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,9 +7,9 @@ function Contact() {
     <div className="row">
         <div className="col-sm-8 col-md-8 col-lg-8"></div>
         <p className="text-center email my-5"><i class="bi bi-envelope-at"></i><br></br> codingwithbenedict99@gmail.com</p>
-        <a className="btn" href="https://github.com/BenedictCodesHere/ben-react-app/blob/main/src/assets/CV/Benedict-McElroy-CV.pdf" target="_blank">CV</a>
-        <Link className="btn" to="https://github.com/BenedictCodesHere/"><i class="bi bi-github"></i></Link>
-        <Link className="btn" to="https://www.linkedin.com/in/benedict-mcelroy-005256230"><i class="bi bi-linkedin"></i></Link>
+        <a className="btn btn-contact" href="https://github.com/BenedictCodesHere/ben-react-app/blob/main/src/assets/CV/Benedict-McElroy-CV.pdf" target="_blank">CV</a>
+        <Link className="btn btn-contact" to="https://github.com/BenedictCodesHere/"><i class="bi bi-github"></i></Link>
+        <Link className="btn btn-contact" to="https://www.linkedin.com/in/benedict-mcelroy-005256230"><i class="bi bi-linkedin"></i></Link>
     </div>
   </div></>
     )
