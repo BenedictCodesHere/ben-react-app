a9efcf29f2d8e5e4e0b56c424573009585af6e1f
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-04-12 12:28:28 +0100

    added skills

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 
 const Header = () => {
-  return (
-    <header className="header mt-5 row w-100">
-      
-          
-      <NavLink className="btn col-sm-4" to="/ben-react-app"><i className="bi bi-house-door"></i></NavLink>
-      
-      
-      <NavLink className="btn col-sm-4" to="/ben-react-app/contact"><i className="bi bi-envelope-at"></i>  <i className="bi bi-github"></i>  <i className="bi bi-linkedin"></i></NavLink>
-      
-      
-      <NavLink  className="btn col-sm-4" to="/ben-react-app/projects"><i className="bi bi-rocket-takeoff"></i>  Projects  <i className="bi bi-rocket-takeoff"></i></NavLink>
+  const clickedButtonStyling = {color: "white", border: "1px solid white"};
+  const [clickedButton, setClickedButton] = useState("");
 
-      <NavLink  className="btn col-sm-4" to="/ben-react-app/skills"><i className="bi bi-screwdriver"></i>  Skills  <i id="rotated" className="bi bi-screwdriver flip-h"></i>  </NavLink>
+  return (
+    <header className="header mt-5 row justify-content-center w-100">
       
+      <NavLink
+        className="btn col-sm-4"
+        to="/ben-react-app"
+        style={clickedButton === "home" ? clickedButtonStyling : { color:"rgb(132, 248, 94)"}}
+        onClick={() => setClickedButton("home")}
+      >
+        <i className="bi bi-house-door-fill"></i>
+      </NavLink>
+      
+      <NavLink
+        className="btn col-sm-4"
+        to="/ben-react-app/contact"
+        style={clickedButton === "contact" ? clickedButtonStyling : {}}
+        onClick={() => setClickedButton("contact")}
+      >
+        <i className="bi bi-envelope-at"></i>  <i className="bi bi-github"></i>  <i className="bi bi-linkedin"></i>
+      </NavLink>
+      
+      <NavLink
+        className="btn col-sm-4"
+        to="/ben-react-app/projects"
+        style={clickedButton === "projects" ? clickedButtonStyling : {}}
+        onClick={() => setClickedButton("projects")}
+      >
+        <i className="bi bi-rocket-takeoff"></i>  Projects  <i className="bi bi-rocket-takeoff"></i>
+      </NavLink>
+
+      <NavLink
+        className="btn col-sm-4"
+        to="/ben-react-app/skills"
+        style={clickedButton === "skills" ? clickedButtonStyling : {}}
+        onClick={() => setClickedButton("skills")}
+      >
+        <i className="bi bi-screwdriver"></i>  Skills  <i className="bi bi-screwdriver icon-flipped"></i>
+      </NavLink>
       
     </header>
   );
 };
-
 
 export default Header;
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -4,7 +4,27 @@ function Skills(){
 
     return (
         <div>
+        <ul>
+            <li>
+           <h2>Front End</h2> 
+           <p>HTML</p>
+           <p>CSS</p>
             <p>Javascript</p>
+            </li>
+            <li>
+           <h2>Back End</h2> 
+            <p>MongoDB</p>
+            <p>SQL</p>
+            </li>
+            <li>
+           <h2>DevOps</h2> 
+            <p>Javascript</p>
+            </li>
+            <li>
+           <h2>Design</h2> 
+            <p>Figma</p>
+            </li>
+            </ul>
         </div>
     )
 } 
