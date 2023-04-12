cd132d2a84bddda135ac3f3b609a2ad65bbb2b72
Benedict McElroy <codingwithbenedict99@gmail.com>
2023-04-12 12:31:19 +0100

    updated skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -10,15 +10,21 @@ function Skills(){
            <p>HTML</p>
            <p>CSS</p>
             <p>Javascript</p>
+            <p>React.js</p>
+            <p>Node</p>
+            <p>Jest/Jest Enzyme</p>
             </li>
             <li>
            <h2>Back End</h2> 
             <p>MongoDB</p>
-            <p>SQL</p>
             </li>
             <li>
            <h2>DevOps</h2> 
-            <p>Javascript</p>
+            <h3>CI/CD</h3>
+            <p>YAML</p>
+            <p>Docker</p>
+            <p>Cloud - AWS</p>
+            <p></p>
             </li>
             <li>
            <h2>Design</h2> 
